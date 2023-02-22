a7e88bfbcedcb92ab7b8149ac89762bbc1ef9c83
dxd429 <dxd429@case.edu>
2023-02-22 16:16:15 -0500

    add unit test

---
+++ inst/unitTests/test_magpie.R
@@ -0,0 +1,60 @@
+##Unit Test using RUnit
+
+#test PowerEval
+test_PowerEval <- function() {
+  if (!require("TBX20BamSubset", quietly = TRUE))
+  BiocManager::install("TBX20BamSubset")
+  library(TBX20BamSubset)
+  power.test <- powerEval(
+    Input.file = as.character(sub(".*/", "", getBamFileList()))[c(1,2,3,3)],
+    IP.file = as.character(sub(".*/", "", getBamFileList()))[c(4,5,6,6)],
+    BamDir = as.character(sub("/[^/]*$", "", getBamFileList()))[1],
+    annoDir = file.path(system.file(package = "magpie"),
+                        "extdata/mm9_chr19.sqlite"),
+    variable = rep(c("Ctrl", "Trt"), each = 2),
+    bam_factor = 1,
+    nsim = 10,
+    N.reps = c(2,3),
+    depth_factor = c(1,2),
+    thres = c(0.01, 0.05, 0.1),
+    Test_method = "exomePeak2"
+  )
+
+  checkTrue(all(names(power.test) == c("1x", "2x")))
+
+  checkEquals(length(power.test), 2)
+  checkEquals(length(power.test$`1x`), 8)
+  checkEquals(length(power.test$`2x`), 4)
+
+}
+
+
+#test quickPower and plot functions
+test_quickPower <- function() {
+  qp <- quickPower(dataset = "GSE94613")
+
+  checkTrue(all(names(qp) == c("1x", "2x", "5x")))
+
+  checkEquals(length(qp), 3)
+  checkEquals(length(qp$`1x`), 8)
+  checkEquals(length(qp$`2x`), 4)
+  checkEquals(length(qp$`5x`), 4)
+
+  p1 <- plotRes(qp, depth_factor = 1, value_option = "Power")
+
+  p2 <- plotAll(qp, depth_factor = 1)
+
+  p3 <- plotStrata(qp, value_option = "FDR")##add na
+
+  p4 <- plotAll_Strata(qp)
+
+  checkTrue(all(c(length(p1),
+                  length(p2),
+                  length(p3),
+                  length(p4)) > 0))
+
+}
+
+
+
+
